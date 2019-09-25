be3328f85811cc32d8f8b12ed4ff01a23f598ef0
leslie <panxiang_work@163.com>
2019-09-25 18:21:39 +0800

    '初始化代码'

---
+++ app/src/main/java/com/studyinghome/doublecircle/MainActivity.java
@@ -0,0 +1,56 @@
+package com.studyinghome.doublecircle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private CircleMenuLayout mCircleMenuLayout;
+
+    private String[] mItemTexts = new String[]{"安全中心 ", "特色服务", "投资理财",
+            "转账汇款", "我的账户", "信用卡", "特色服务", "投资理财", "特色服务", "投资理财"};
+    private int[] mItemImgs = new int[]{R.drawable.home_mbank_1_normal,
+            R.drawable.home_mbank_2_normal, R.drawable.home_mbank_3_normal,
+            R.drawable.home_mbank_4_normal, R.drawable.home_mbank_5_normal,
+            R.drawable.home_mbank_6_normal,
+            R.drawable.home_mbank_2_normal, R.drawable.home_mbank_3_normal,
+            R.drawable.home_mbank_2_normal, R.drawable.home_mbank_3_normal};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //自已切换布局文件看效果
+        setContentView(R.layout.activity_main);
+
+        mCircleMenuLayout = (CircleMenuLayout) findViewById(R.id.id_menulayout);
+        mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
+
+
+        mCircleMenuLayout.setOnMenuItemClickListener(new CircleMenuLayout.OnMenuItemClickListener() {
+
+            @Override
+            public void itemClick(View view, int pos) {
+                Toast.makeText(MainActivity.this, mItemTexts[pos],
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void itemCenterClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "you can do something just like ccb  ",
+                        Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+
+            }
+        });
+
+    }
+
+}
